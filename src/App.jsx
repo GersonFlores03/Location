d79ba6f4fe621fe4531a8ade9b560b90c2a7c305
Gerson Flores Narciso <110065310+Gerson12-afk@users.noreply.github.com>
2023-03-08 18:32:37 -0500

    wather

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState ,useEffect } from 'react'
 import axios from 'axios'
 import reactLogo from './assets/react.svg'
+import gerflores from "./assets/gerflores.png"
 import './App.css'
 
  function App() {
@@ -19,22 +20,18 @@ import './App.css'
    const crd = pos.coords;
    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=46dc136f9103941630256983c3a4a97a`)
    .then(res=> setisClima(res.data))
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
+   
 }
 
 
 function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
+ 
 }
 
 navigator.geolocation.getCurrentPosition(success, error);
 
  },[])
- console.log(isclima)
-
+ 
 
  const  C = (isclima.main?.temp-273.15).toFixed(2)
  const  F = C * 1.8 + 32
@@ -42,56 +39,76 @@ navigator.geolocation.getCurrentPosition(success, error);
  const Boton1 = ()=>{
      setCelsius(!iscelsius)
  }
-//Imagen ramdon No terminda dudas
-/*const RandomImg = [];
-RandomImg[0]= "./assets/beautiful-sunset-beach-landscape-with-boat.jpg"
-RandomImg[1]= "./assets/black-rain-abstract-dark-power.jpg"
-window.onload = function (){
-  const Random = Math.floor(Math.random()* RandomImg.length)
-  document.body.style.backgroundImage = `url(${RandomImg[Random]})`
-}*/
 
-//Funcion de loader
+
 document.addEventListener("DOMContentLoaded", ()=>{
-      loadComponet();
+  loadComponet();
 })
 
 
 
 
   return (
-    <div className='Clima'>
-    <div className="App">
-      <div className='CardClima'>
-        <div className='ClimaVariantes'>
+    <div className='Contendor-Wather'>
+       <div className='Link'>
+        <h2> Wather App una aplicación Meteorología </h2>
+        <button className='Link-Wather'> <a href="#Wather"> Pruebalo... </a>  </button> 
+        <div className='SobreMi'>
+         <div className='Img'>
+             <img src={gerflores} alt="" />
+         </div>
+         <div className='Texto'>
+             <h2>
+              Soy Gerson Elmer Flores Narciso he creado esta
+              app de Meteorología con la ayuda de un API , 
+              usando buenas practicas y buena estructura.
+             </h2>
+             <div className='Redes'>
+                <span className='Circulo'> <a href="https://www.linkedin.com/in/gerson-flores-narciso-52628b256/"> <i className='bx bxl-linkedin-square linke' > </i> </a>  </span>
+             
+             <span className='Circulo'> <a href="https://gerson-flores-a2a3e8.netlify.app/"> <i className='bx bxs-briefcase-alt-2 portafolio'></i> </a> </span>
+             </div>
+         </div>
+      </div>
+      </div>
+      
+      
+     <div id='Wather'className="Wather-Container">
+        <div className='Location'>
+        <h1 className='title' >Wheather App</h1>
+        <h2>{isclima?.name} ,{isclima.sys?.country} </h2>
+        </div>
+      <div className='ClimaVariantes'>
+        <section className='Image-Datos' > 
         <img src={`http://openweathermap.org/img/wn/${isclima.weather?.[0].icon}@2x.png`} alt="" />
+        <div className='BotonGrados'>
+        <h2> { iscelsius? C: F} {" "} {iscelsius ? " °C " : "°F"} </h2>
+        <button className='Buton' onClick={Boton1}>Degrees°C/°F</button>
+        </div>
+        </section>
+        <section className='Text-Contenido'> 
           <ul>
-            <li className='ListaUno'> 
-            <i className="fa-brands fa-cloudflare"></i> {" "}
+            <li className='Lista'> 
+            <i className="fa-brands fa-cloudflare viento"></i> {" "}
              Wind Speed:  <span> {isclima.wind?.speed }  m/s </span>
             </li>
-            <li className='ListaDos'>
-              <i className="fa-solid fa-cloud"></i> {" "}
+            <li className='Lista'>
+              <i className="fa-solid fa-cloud cloud"></i> {" "}
               Clouds: <span> {isclima.clouds?.all}  % </span>
             </li>
-            <li className='ListaTres'>
-              <i className="fa-solid fa-temperature-three-quarters"></i> {""}
+            <li className='Lista'>
+              <i className="fa-solid fa-temperature-three-quarters temp "></i> {""}
              Pressure:  <span> {isclima.main?.pressure} mbr </span>
             </li>
           </ul>
+          </section>
         </div>
-        <div className='Location'>
-        <h1>Wheather App</h1>
-        <h2>{isclima?.name} ,{isclima.sys?.country} </h2>
-        </div>
+     
        
-        <div className='BotonGrados'>
-        <h2> { iscelsius? C: F} {" "} {iscelsius ? " °C " : "°F"} </h2>
-        <button onClick={Boton1}>Degrees°C/°F</button>
-        </div>
+      
        
        
-      </div>
+      
     </div>
    
     <div className='loader' id='loader'>
